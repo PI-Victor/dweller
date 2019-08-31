fa34eaf8b0fa9c22133315537753f81349680831
Victor Palade <victor@cloudflavor.io>
2019-08-31 18:28:34 +0200

    cli: re-arrange cli struct
    This will reorg the cli UX

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,14 @@ extern crate serde_derive;
 #[macro_use]
 extern crate log;
 extern crate env_logger;
+extern crate failure;
 
 mod util;
 mod providers;
 
 use clap::{App, AppSettings, Arg, SubCommand};
-use util::Configuration;
+
+use util::open_file;
 
 const VERSION: &str = "v0.1.0-alpha";
 
@@ -21,44 +23,48 @@ const ASCIIART: &str = r#"
  | '_ ` _ \ / _ \ / _` | |
  | | | | | | (_) | (_| | |
  |_| |_| |_|\___/ \__,_|_|
-                          
-Manage opinionated infrastructure for private cloud"                          
+
+Manage public/private cloud as declarative resources"
 "#;
 
 fn main() {
-    let provs: [&str; 1] = ["libvirt"];
+    let filename = Arg::with_name("filename")
+        .help("resource file to apply")
+        .long("filename")
+        .short("f")
+        .required(true)
+        .takes_value(true);
+
     let matches = App::new("moai")
         .author("Cloudflavor Org")
         .version(VERSION)
         .about(ASCIIART)
         .setting(AppSettings::SubcommandRequiredElseHelp)
-        .arg(Arg::with_name("v")
+        .arg(Arg::with_name("verbosity")
                 .help("verbosity level 0-4")
+                .long("verbosity")
                 .short("v")
                 .multiple(true))
+        .arg(Arg::with_name("workspace")
+                .help("choose a workspace")
+                .long("workspace")
+                .short("w")
+                .global(true))
         .subcommand(SubCommand::with_name("create")
                 .display_order(1)
-                .about("provision a new cluster"))
-        .subcommand(SubCommand::with_name("modify")
+                .about("create new resource(s)")
+                .arg(&filename))
+        .subcommand(SubCommand::with_name("apply")
                 .display_order(2)
-                .about("modify an existing cluster"))
+                .about("modify (patch) existing resources")
+                .arg(&filename))
         .subcommand(SubCommand::with_name("delete")
                 .display_order(3)
-                .about("delete a cluster"))
-        .arg(Arg::with_name("provider")
-                .help("specify which provider to use. See the docs for supported providers")
-                .default_value("libvirt")
-                .possible_values(&provs))
-        .arg(Arg::with_name("config")
-                .short("c")
-                .long("config")
-                .value_name("JSON, TOML, YAML, HJSON, INI - configuration")
-                .help("Path to config file")
-                .takes_value(true)
-                .required(true))
+                .about("delete existing resources")
+                .arg(&filename))
         .get_matches();
 
-    let log_level = match matches.occurrences_of("v") {
+    let log_level = match matches.occurrences_of("verbosity") {
         0 => log::LevelFilter::Error,
         1 => log::LevelFilter::Warn,
         2 => log::LevelFilter::Info,
@@ -69,11 +75,17 @@ fn main() {
     env_logger::Builder::from_default_env()
         .filter(Some(module_path!()), log_level)
         .init();
-    let mut config = Configuration::default();
 
-    if let Some(matches) = matches.subcommand_matches("new") {
-        config = Configuration::new(matches.value_of("config").unwrap())
-            .unwrap();
+    let mut file_path = matches.value_of("filename");
+
+    match matches.subcommand_name().unwrap() {
+        "create" => create_resources(file_path.unwrap()),
+        "apply" => debug!("apply"),
+        "delete" => debug!("delete"),
+        _ => ()
     }
-    debug!("Loaded configuration: {:?}", config);
+}
+
+fn create_resources(file_path: &str) {
+    let deserialize = open_file(file_path).unwrap();
 }
