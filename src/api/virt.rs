6e8aeb386a25c97b564421a652bba47280981b98
Victor Palade <victor@cloudflavor.io>
2019-09-02 10:17:29 +0200

    spec: add domain, network and pool initial spec.

--- src/api/virt.rs
+++ src/api/virt.rs
@@ -1,4 +1,38 @@
 #[derive(Serialize, Deserialize, Debug)]
+enum Kind {
+    Domain,
+    Pool,
+    Network,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Network<K> {
+    pub kind: K,
+    pub name: String 
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct StoragePool<K> {
+    pub kind: K,
+    pub name: String,
+    pub allocation: u16,
+    pub capacity: u16,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Domain<K> {
+    pub kind: K,
+    pub domain_type: String,
+    pub name: String,
+    pub vcpu: u8,
+    pub memory: u8,
+    pub disks: StorageDisks,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct StorageDisks {}
+
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Libvirt {
     pub uri: String
 }
